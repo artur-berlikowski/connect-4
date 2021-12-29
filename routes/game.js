5000c617ab3f67ec7f4a4c840cb1689a30322ec6
artur-berlikowski <a.berlikowski@gmail.com>
2021-12-29 22:47:05 +0100

    Styling for navbar. Changed logo and added new router paths for the menu options.

--- routes/game.js
+++ routes/game.js
@@ -4,7 +4,15 @@ const res = require('express/lib/response');
 let router = express.Router();
 
 router.get('/', (req, res) => {
+  res.send('game');
+});
 
+router.get('/rules', (req, res) => {
+  res.send('rules');
+});
+
+router.get('/leaderboard', (req, res) => {
+  res.send('leaderboard');
 });
 
 module.exports = router;
