0f38b4b0f3cc32c725f8c6106803fd081af3e654
artur-berlikowski <a.berlikowski@gmail.com>
2022-01-01 10:43:33 +0100

    Applied more styling. Added some welcome text to the main page. Created a button that takes the user to the game page.

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,12 @@ const res = require('express/lib/response');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    partials: {
+      nav: 'partials/navbar',
+      main: 'partials/home'
+    }
+  });
 });
 
 module.exports = router;
